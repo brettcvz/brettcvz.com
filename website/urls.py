c79475890eaff7ead872d4828ec6a48e0447046b
Brett van Zuiden <brettcvz@gmail.com>
2015-01-02 18:57:45 -0800

    Initial styling for homepage

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'website.views.home', name='home'),
@@ -9,4 +12,4 @@ urlpatterns = patterns('',
     url(r'^projects/', include('projects.urls', namespace='projects')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
