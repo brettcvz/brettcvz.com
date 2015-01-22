94639f9cbc5ba808f528d54d1f9219322afca7c6
Brett van Zuiden <brettcvz@gmail.com>
2015-01-21 21:52:24 -0800

    Added the ability for admins to see unpublished posts

--- posts/views.py
+++ posts/views.py
@@ -4,15 +4,19 @@ from .models import Post
 
 
 def index(request):
+    posts = Post.objects.filter(visible=True)
+    if request.user.has_perm('posts.view_hidden'):
+        posts = Post.objects.all()
+
     return render(request, 'posts/index.html', {
-        'posts': Post.objects.filter(visible=True),
-        'top_posts': Post.objects.filter(visible=True, top_post=True),
+        'posts': posts,
+        'top_posts': posts.filter(top_post=True),
     })
 
 
 def post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    if not post.visible:
+    if not post.visible and not request.user.has_perm('posts.view_hidden'):
         raise Http404("Post not found")
 
     return render(request, 'posts/post.html', {
