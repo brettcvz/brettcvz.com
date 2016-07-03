ecb40290c66bb485e70b6425fa0bbed3d07866e2
Brett van Zuiden <brettcvz@gmail.com>
2016-07-03 16:11:45 -0700

    Compiled the Linux kernel!
    Updated about page, because it was a fun milestone :)

--- static/js/about.js
+++ static/js/about.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", function(){
     //set of events, key is the dot position, value is the text
     //associated with it (optionally with positions)
     var events = {
-        2016.32: "Launched Clever Badges",
-        2016.15: {
-            2016.18: "Began tech volunteering",
-            2016.10: "at Alpha Public Schools"
+        2016.5: {
+          2016.50: "First time compiling",
+          2016.42: "the Linux kernel"
         },
+        2016.32: "Launched Clever Badges",
         2015.65: "Started playing DnD",
         2015.45: {
             2015.45: "Created the Schools",
