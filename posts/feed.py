78a3a2624363f2e3b419abdcd0a1bfd93cac80aa
Brett van Zuiden <brett.vanzuiden@clever.com>
2020-01-19 23:08:14 -0800

    Added RSS for posts

---
+++ posts/feed.py
@@ -0,0 +1,16 @@
+from django.contrib.syndication.views import Feed
+from models import Post
+
+class LatestPostsFeed(Feed):
+    title = "Latest posts from Brettcvz.com"
+    link = "/posts/"
+    description = "I write on things that interest me: product management, startups, and general tech trends."
+
+    def items(self):
+        return Post.objects.order_by('-pubdate')[:5]
+
+    def item_title(self, post):
+        return post.title
+
+    def item_description(self, post):
+        return post.clean_preview()
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 import views
+import feed
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^rss$', feed.LatestPostsFeed(), name='rss'),
     url(r'^(?P<post_id>\d+)[-\w]*$', views.post, name='post'),
 ]
