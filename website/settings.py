6ceee5093835eec201093532885a5709e1e045f0
Brett van Zuiden <brettcvz@gmail.com>
2024-03-05 20:56:34 -0800

    Something broke again...

--- website/settings.py
+++ website/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 SECRET_KEY = os.getenv("DJANGO_SECRET")
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
